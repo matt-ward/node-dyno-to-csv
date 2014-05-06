4eb532ee5f767d9959c5148a915aa431d8214ba1
Matt Ward <Matt@GreenMountainSoftware.com>
2014-05-06 17:00:09 -0400

    finished query

--- test/index.js
+++ test/index.js
@@ -3,18 +3,43 @@ var should = require('chai').should()
     ,  DynoToCSV = require('../index.js')
     ,  configure = DynoToCSV.configure
     ,  exportData = DynoToCSV.exportData
+    ,  describeTable = DynoToCSV.describe
     ,  alias = DynoToCSV.aliasAttributes;
 
 
 describe('#configure', function() {
     it('Configures the module with an instance of AWS Dynamodb and will throw an exception if it is not of type AWS.DynamoDB', function() {
-        var instance = configure(new AWS.DynamoDB());
+        configure({dynamodbInstance: new AWS.DynamoDB()});
 
-    })
+    });
+
+    it('Accepts AWS Credentials and instantiates a valid AWS session', function() {
+    });
 });
+
+describe('#exportData', function() {
+    this.timeout(60000);
+
+
+    it('Exports data', function(done) {
+    });
+
+
+
+});
+
+describe('#describeTable()', function() {
+    it('Describes a dynamo table', function(done) {
+
+
+
+    });
+})
+
+
 
 describe('#AliasAttributes', function() {
     it('Aliases DynamoDB Attributes', function() {
         return true;
     });
-})
+});
