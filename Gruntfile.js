8fc48ace89ec3b2465588ca3aff7f07161b54a39
Matt Ward <Matt@GreenMountainSoftware.com>
2014-05-07 12:53:05 -0400

    convert to csv

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
     // Add the grunt-mocha-test tasks.
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.initConfig({
         // Configure a mochaTest task
@@ -12,9 +13,32 @@ module.exports = function(grunt) {
                 },
                 src: ['test/**/*.js']
             }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                browser: true,
+                node: true,
+                globals: {
+                    jQuery: true
+                }
+            },
+            uses_defaults: ['lib/**/*.js'],
+            with_overrides: {
+                options: {
+                    curly: false,
+                    undef: true
+
+                },
+                files: {
+                    src: ['lib/**/*.js']
+                }
+            }
         }
     });
 
-    grunt.registerTask('default', 'mochaTest');
+    grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
